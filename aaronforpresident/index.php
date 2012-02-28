cee1e65cd7dfa8eda4cfba548df0928c9c1c3347
Gary Fitzpatrick <fityp12@gmail.com>
2012-02-28 11:25:05 +0000

    working

--- aaronforpresident/index.php
+++ aaronforpresident/index.php
@@ -1,3 +1,3 @@
 <?php
-	echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president"></img></body></head>';
+	echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
 ?>
