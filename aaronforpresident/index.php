65da78fb20713d3b786174e5ec5553a985890d8d
Gary Fitzpatrick <fityp12@gmail.com>
2012-02-28 13:49:08 +0000

    testing api

--- aaronforpresident/index.php
+++ aaronforpresident/index.php
@@ -10,7 +10,7 @@
 
 	if($response['data']){
 		echo "I Like";
-	}else{
+	} else {
 		echo "I dont like"
 	}
 	//echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
