7e3df9054237b74209095e65f6ff14e6a37ec2d8
Gary Fitzpatrick <fityp12@gmail.com>
2012-02-28 11:50:07 +0000

    adding facebook

--- aaronforpresident/index.php
+++ aaronforpresident/index.php
@@ -1,24 +1,3 @@
 <?php
-	require 'src/facebook.php';
-	
-	$facebook = new Facebook(array(
-		'appId' => '198588890243186',
-		'secret' => '8e870b4559171dad72c5201b7a0d6c4b'
-	));
-	
-	$user = $facebook->getUser();
-
-	if($user){
-		try {
-			$likes = $facebook->api("/me/likes/313057122077593");
-			if(!empty($likes['data'])){
-				echo "I likes!";
-			}else{
-				echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
-			}
-		} catch (FacebookApiException $e){
-			print_r($e);
-			$user = null;
-		}
-	}
+	echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
 ?>
