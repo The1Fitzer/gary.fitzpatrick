faa7d7d44e24e2c20f9a5335489b9d9247e36c61
Gary Fitzpatrick <fityp12@gmail.com>
2012-02-28 13:44:16 +0000

    testing api

--- aaronforpresident/index.php
+++ aaronforpresident/index.php
@@ -7,12 +7,11 @@
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 	$response = json_decode(curl_exec($ch), true);
-	print_r($response['data']);	
 	
-/*	if(!empty($response['data'])){
+	if(!empty($response['data'])){
 		echo "I Like";
 	}else{
 		echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
 		echo "I Don't Like"
-	}*/
+	}
 ?>
