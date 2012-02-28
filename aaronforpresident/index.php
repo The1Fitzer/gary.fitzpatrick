ccb810f61db5061607a6d1b7181539300a77a455
Gary Fitzpatrick <fityp12@gmail.com>
2012-02-28 13:37:24 +0000

    testing api

--- aaronforpresident/index.php
+++ aaronforpresident/index.php
@@ -1,3 +1,16 @@
 <?php
-	echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
+
+	$url = 'https://graph.facebook.com/me/likes/313057122077593&access_token=AAACEdEose0cBANeai8Q642H0EBt4BDaZAvv58wTSEbPHchZBF53U0q4HHGD2tZCuhlyRSDUrJALwn9CZAXmZAxNQZAjZC0QsBJFbmI9jvjTeQZDZD';
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	$response = json_decode(curl_exec($ch));
+	
+	if(!empty($response['data'])){
+		echo "I Like";
+	}else{
+		echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
+	}
 ?>
