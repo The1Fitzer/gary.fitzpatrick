73d9199cf8e84d1c8172a8adf7e52d3b151ee210
Gary Fitzpatrick <fityp12@gmail.com>
2012-02-28 14:08:07 +0000

    working facebook

--- aaronforpresident/index.php
+++ aaronforpresident/index.php
@@ -1,5 +1,4 @@
 <?php
-
 	$url = 'https://graph.facebook.com/me/likes/313057122077593&access_token=AAACEdEose0cBANeai8Q642H0EBt4BDaZAvv58wTSEbPHchZBF53U0q4HHGD2tZCuhlyRSDUrJALwn9CZAXmZAxNQZAjZC0QsBJFbmI9jvjTeQZDZD';
 
 	$ch = curl_init($url);
@@ -7,11 +6,11 @@
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 	$response = json_decode(curl_exec($ch), true);
-
-	if($response['data']){
-		echo "I Like";
-	} else {
-		echo "I dont like"
+	
+	if(empty($response['data'])){
+		echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
+	}else{
+		echo "Manifesto Coming Soon";
 	}
-	//echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
+	
 ?>
