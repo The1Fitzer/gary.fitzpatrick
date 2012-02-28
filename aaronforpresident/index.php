c4e380fe46c8d8572d38da98fe7fa52deea90e72
Gary Fitzpatrick <fityp12@gmail.com>
2012-02-28 11:47:52 +0000

    adding facebook

--- aaronforpresident/index.php
+++ aaronforpresident/index.php
@@ -17,7 +17,7 @@
 				echo '<html><head><title>Aaron McGee for President</title></head><body><img src="aaron.jpg" alt="aaron for president" width ="500px" height="700px"></img></body></head>';
 			}
 		} catch (FacebookApiException $e){
-			error_log($e);
+			print_r($e);
 			$user = null;
 		}
 	}
