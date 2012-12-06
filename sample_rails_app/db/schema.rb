9798109e47e64fb93ce0c5388fe91485fc122ee8
The1Fitzer <fityp12@gmail.com>
2012-12-06 00:42:31 +0000

    forgot to add schema

--- sample_rails_app/db/schema.rb
+++ sample_rails_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121124214019) do
+ActiveRecord::Schema.define(:version => 20121206003820) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(:version => 20121124214019) do
     t.string   "hashed_password"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "salt"
   end
 
 end
