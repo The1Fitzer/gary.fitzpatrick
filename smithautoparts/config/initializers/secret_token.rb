55967accb9caa835608048c3d4a1470fbeaf387e
The1Fitzer <fityp12@gmail.com>
2012-12-08 19:48:11 +0000

    Smith Auto Parts

---
+++ smithautoparts/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smithautoparts::Application.config.secret_token = '6c0bbec8e95ead5e303d168008289da2c4de72d9b3640866ff1280408c95575954d71f26ecd562a50a97195f9e6e4aa16433cc623ec34d1dd03bbe2323e9153e'
